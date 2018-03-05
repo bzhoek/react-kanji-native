c1c7e27eb9abc19eeb112e3f670dca0b5ad6776d
Bas van der Hoek <bas@hoek.com>
2018-03-06 00:22:29 +0100

    extracted kanji list

--- App.js
+++ App.js
@@ -1,8 +1,6 @@
 import React from 'react'
-import {List, FlatList, NavigatorIOS, StyleSheet, Text, View, SafeAreaView} from 'react-native'
-import {ListView} from 'realm/react-native'
-import KanjiService from './src/KanjiService'
-import Kanji from './components/Kanji'
+import {List, NavigatorIOS, StyleSheet} from 'react-native'
+import KanjiList from './components/KanjiList'
 
 export default class App extends React.Component {
 
@@ -17,57 +15,6 @@ export default class App extends React.Component {
       />
     );
   }
-
-  flatList() {
-    return <SafeAreaView style={styles.container}>
-      <Text>Shake your phone to open the developer menu.</Text>
-      <FlatList
-        data={this.state.data}
-        renderItem={({item}) => <Text>Hallo</Text>}/>
-    </SafeAreaView>
-  }
-}
-
-let ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
-
-class KanjiList extends React.Component {
-
-  constructor(props) {
-    super(props)
-    this.state = {
-      dataSource: ds
-    }
-  }
-
-  componentDidMount() {
-    let query = KanjiService.findAll()
-    this.setState({
-      dataSource: ds.cloneWithRows(query)
-    })
-  }
-
-  render() {
-    return <View style={styles.container}>
-      <Text>Shake your phone to open the developer menu.</Text>
-      <ListView
-        dataSource={this.state.dataSource}
-        renderRow={this.renderItem.bind(this)}/>
-    </View>
-  }
-
-  _onForward(item) {
-    console.log(`Hallo, ${item.literal}`)
-    this.props.navigator.push({
-      component: Kanji,
-      title: item.literal,
-      passProps: {drawing: item.drawing.replace(/(\r\n|\n|\r)/gm, "")},
-    });
-  }
-
-  renderItem(item) {
-    return <Text onPress={() => this._onForward(item)}>{item.literal} - {item.meaning}</Text>
-  }
-
 }
 
 const styles = StyleSheet.create({
---
+++ components/KanjiList.js
@@ -0,0 +1,54 @@
+import React from "react"
+import {StyleSheet, Text, View} from 'react-native';
+import Kanji from "./Kanji"
+import KanjiService from "../src/KanjiService"
+import {ListView} from "realm/react-native/index"
+
+let ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1.literal !== r2.literal});
+
+export default class KanjiList extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      dataSource: ds
+    }
+  }
+
+  componentDidMount() {
+    let query = KanjiService.findAll()
+    this.setState({
+      dataSource: ds.cloneWithRows(query)
+    })
+  }
+
+  render() {
+    return <View style={styles.container}>
+      <Text>Shake your phone to open the developer menu.</Text>
+      <ListView
+        dataSource={this.state.dataSource}
+        renderRow={this.renderItem.bind(this)}/>
+    </View>
+  }
+
+  _onForward(item) {
+    console.log(`Hallo, ${item.literal}`)
+    this.props.navigator.push({
+      component: Kanji,
+      title: item.literal,
+      passProps: {drawing: item.drawing.replace(/(\r\n|\n|\r)/gm, "")},
+    });
+  }
+
+  renderItem(item) {
+    return <Text onPress={() => this._onForward(item)}>{item.literal} - {item.meaning}</Text>
+  }
+
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+});
