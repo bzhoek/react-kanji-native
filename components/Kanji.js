790526b40d9e387750d4e16a4ec24683328b4bec
Bas van der Hoek <bas@hoek.com>
2018-03-05 17:06:16 +0100

    inject svg drawing and start animation

--- components/Kanji.js
+++ components/Kanji.js
@@ -4,10 +4,15 @@ import html from './Kanji.html'
 
 class Kanji extends Component {
 
+  constructor(props, context) {
+    super(props, context);
+  }
+
   render() {
+    let {drawing} = this.props
     return (
       <WebView source={html} style={{flex: 1}}
-      />
+               injectedJavaScript={`document.getElementById('kanji-strokes').innerHTML = '${drawing}'; animate_paths()`}/>
     );
   }
 }
