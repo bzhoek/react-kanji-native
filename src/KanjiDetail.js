ac789f6077242f41b0bf6bc632acea086482b28c
Bas van der Hoek <bas@hoek.com>
2018-03-30 13:24:09 +0200

    deconstruct literal

--- src/KanjiDetail.js
+++ src/KanjiDetail.js
@@ -5,10 +5,10 @@ import html from './Kanji.html'
 export default class KanjiDetail extends Component {
   render() {
     let drawing = this.props.drawing.replace(/(\r\n|\n|\r)/gm, "")
-    let {meaning} = this.props
+    let {literal, meaning} = this.props
     return (
       <View style={{flex: 1}}>
-        <WebView source={html} style={{flex: 1}} key={this.props.literal}
+        <WebView source={html} style={{flex: 1}} key={literal}
           injectedJavaScript={`document.getElementById('kanji-strokes').innerHTML = '${drawing}'; animate_paths()`}/>
         <Text style={{flex: 1}}>{meaning}</Text>
       </View>
